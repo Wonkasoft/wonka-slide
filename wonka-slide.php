3afc9347491a575eab8bab020ee11c7f215986be
MrLister <rlister@wonkasoft.com>
2017-12-12 18:17:30 -0800

    MrLister 12-12-2017 6:17pm

--- wonka-slide.php
+++ wonka-slide.php
@@ -20,14 +20,40 @@
  */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+// For setting up plugin at activation
 register_activation_hook( __FILE__, 'wonka_slide_activator' );
 function wonka_slide_activator() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wonka-slide-activator.php';
 	wonka_slide_activator::activate();
 }
 
+// For deleting all information
 register_deactivation_hook( __FILE__, 'wonka_slide_deactivator' );
 function wonka_slide_deactivator() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wonka-slide-deactivator.php';
 	wonka_slide_deactivator::deactivate();
 }
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'inc/class-wonka-slide.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wonka_slide() {
+
+	$plugin = new Wonka_Slide();
+	$plugin->run();
+
+}
+
+run_wonka_slide();
